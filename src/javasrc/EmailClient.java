ac107eaf4b2484821a9178a3e5017ea1f88f0013
Tyler Grant <tylergrant13@familysearch.org>
2018-08-21 15:26:35 -0600

    Make use of static functions.

--- src/javasrc/EmailClient.java
+++ src/javasrc/EmailClient.java
@@ -8,39 +8,28 @@ import javax.swing.*;
 
 public class EmailClient
 {
-  private static HashMap<String, String> emails;
-  private static ArrayList<FileEntryPair> finalEntries;
-  private static ArrayList<String> missingServices;
-  private static String writeToCSV;
-  private static int currentSum = 0;
-  private static GUIFrame GUI;
-  private static PrintWriterHelper PWHelper;
-
-
   // ====================================================================================
   // ================================== MAIN FUNCTION ===================================
   // ====================================================================================
 
   public static void main(String[] args)
   {
-    GUI = new GUIFrame();
-    PWHelper = new PrintWriterHelper();
     StaticLists.fillServices();
     StaticLists.fillDaysOfWeek();
-    GUI.createView();
+    GUIFrame.createView();
   }
 
   // ====================================================================================
   // ================================= CLICK LISTENER ===================================
   // ====================================================================================
 
-  public static void doTheClick()
+  static void doTheClick()
   {
     // GRAB TEXT FIELD VALUES
-    String email = GUI.getEmailTextField().getText();
-    char[] passwordArray = GUI.getPasswordTextField().getPassword();
+    String email = GUIFrame.getEmailTextField().getText();
+    char[] passwordArray = GUIFrame.getPasswordTextField().getPassword();
     String password = String.valueOf(passwordArray);
-    writeToCSV = GUI.getDestinationTextField().getText();
+    String writeToCSV = GUIFrame.getDestinationTextField().getText();
 
     // ENSURE THAT THE DESTINATION TEXT FIELD IS A CSV FILE
     if(!writeToCSV.endsWith(".csv"))
@@ -48,22 +37,23 @@ public class EmailClient
 
     // RETRIEVE THE EMAILS FROM OUTLOOK USING THE USERNAME AND PASSWORD
     OutlookReader outlookReader = new OutlookReader(email, password);
-    emails = outlookReader.retrieveEmails();
+    HashMap<String, String> emails = outlookReader.retrieveEmails();
 
     // ENSURE THAT ALL OF THE SERVICES WERE IMPORTED
-    missingServices = new ArrayList<>();
+    ArrayList<String> missingServices = new ArrayList<>();
     if(emails.size() != StaticLists.services.size())
       MissingServicesUtil.check(emails, missingServices);
 
     // PARSE THE EMAILS INTO USABLE CSV FILES
     CSVParser parser = new CSVParser();
-    finalEntries = parser.parseCSVs(emails);
+    ArrayList<FileEntryPair> finalEntries = parser.parseCSVs(emails);
 
     // OPEN UP A WRITER TO WRITE TO THE FILE
-    PrintWriter pw = PWHelper.initializePrintWriter(writeToCSV);
+    PrintWriter pw = PrintWriterHelper.initializePrintWriter(writeToCSV);
 
     // WRITE THE RESULTS TO THE OUTPUT FILE
-    PWHelper.writeToDestinationCSV(pw, finalEntries, currentSum);
+    int currentSum = 0;
+    PrintWriterHelper.writeToDestinationCSV(pw, finalEntries, currentSum);
     pw.flush(); pw.close();
 
     // EXIT
